fc695b7abd0cc1bd89532525460aae72d80389aa
manoj1711 <113223927+manoj1711@users.noreply.github.com>
2023-12-01 20:09:25 +0530

    Update BIrd Fly or Not.pl

--- BIrd Fly or Not.pl
+++ BIrd Fly or Not.pl
@@ -1,16 +1,22 @@
-bird(swan).
-bird(sparrow).
-bird(eagle).
-bird(duck).
-bird(crow).
-bird(ostrich).
-bird(puffin).
-bird(swan).
-bird(albatross).
-bird(starling).
-bird(owl).
-bird(kingfisher).
-bird(thrush).
+% Facts representing the database of birds and their flying capabilities
+bird(swan, can_fly).
+bird(sparrow, can_fly).
+bird(eagle, can_fly).
+bird(duck, can_fly).
+bird(crow, can_fly).
+bird(ostrich, cannot_fly).
+bird(puffin, can_fly).
+bird(albatross, can_fly).
+bird(starling, can_fly).
+bird(owl, can_fly).
+bird(kingfisher, can_fly).
+bird(thrush, can_fly).
 
+% Query to check if a particular bird can fly
+can_fly(Bird) :-
+    bird(Bird, can_fly),
+    write(Bird), write(' can fly.'), nl.
 
-can_fly(X):- bird(X).
+can_fly(Bird) :-
+    bird(Bird, cannot_fly),
+    write(Bird), write(' cannot fly.'), nl.
